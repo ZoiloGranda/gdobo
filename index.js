698827a9a45a4787f362988500ee451ce4943917
Zoilo Granda <zgranda@gmail.com>
2019-10-28 13:11:30 -0400

    Added chalk logs Improved code

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const folder = process.env.FOLDER_TO_UPLOAD;
 const fs = require('fs');
 const {google} = require('googleapis');
 const Promise = require('bluebird');
 const readline = require('readline');
 const chalk = require('chalk');
- 
+
+require('dotenv').config();
+const folder = process.env.FOLDER_TO_UPLOAD;
+console.log(folder);
 var auth;
 
 function startProcess(authData) {
@@ -14,25 +16,62 @@ function startProcess(authData) {
 
 function listAllFiles() {
  fs.readdir(folder, (err, filenames) => {
-  console.log(chalk.blue(filenames));
+  console.log(chalk.gray(filenames));
   sendFilesInArray(filenames)
  });
 }
 
 function sendFilesInArray(filenames) {
+ console.log(filenames);
  Promise.reduce(filenames, function(total, currentFile) {
+  console.log({currentFile});
   return upload(currentFile).then(function(data) {
-   console.log(data.status);
-   console.log(data.statusText);
-   return ;
+   console.log(chalk.green(`${data.status} ${data.statusText}`));
+   return;
   });
  }).then(function(total) {
-  console.log('SUCCESS ALL FILES');
+  console.log(chalk.bgGreen('SUCCESS ALL FILES'));
  }).catch(function(err) {
   console.log('ERROR');
   console.log(err);
  });
 }
+function upload(filename) {
+ return new Promise(function(resolve, reject) {
+  console.log(chalk.inverse(`UPLOADING: ${filename}`));
+  const fileSize = fs.statSync(folder+filename).size;
+  const drive = google.drive({version: 'v3', auth});
+  var fileMetadata = {
+   'name': filename
+  };
+  var media = {
+   mimeType: 'audio/mpeg',
+   body: fs.createReadStream(folder+filename)
+  };
+  drive.files.create({
+   resource: fileMetadata,
+   media: media,
+   fields: 'id'
+  },{
+   onUploadProgress: evt => {
+    const progress = (evt.bytesRead / fileSize) * 100;
+    readline.clearLine(process.stdout, 0)
+    readline.cursorTo(process.stdout, 0, null)
+    process.stdout.write(chalk.inverse(`${Math.round(progress)}% complete`));
+   },
+  }, function (err, file) {
+   if (err) {
+    console.log(`ERROR WITH ${filename}`);
+    console.error(err);
+    reject(err)
+   } else {
+    console.log(chalk.green(`\nFile saved: ${filename}`));
+    resolve(file)
+   }
+  })
+ })
+}
+
 // /**
 // * Lists the names and IDs of up to 10 files.
 // * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
@@ -56,55 +95,17 @@ function sendFilesInArray(filenames) {
 //  });
 // }
 // 
-function upload(filename) {
- return new Promise(function(resolve, reject) {
-  console.log(`UPLOADING: ${filename}`);
-  const fileSize = fs.statSync(folder+filename).size;
-  const drive = google.drive({version: 'v3', auth});
-  var fileMetadata = {
-   'name': filename
-  };
-  var media = {
-   mimeType: 'audio/mpeg',
-   body: fs.createReadStream(folder+filename)
-  };
-  drive.files.create({
-   resource: fileMetadata,
-   media: media,
-   fields: 'id'
-  },{
-   // Use the `onUploadProgress` event from Axios to track the
-   // number of bytes uploaded to this point.
-   onUploadProgress: evt => {
-    const progress = (evt.bytesRead / fileSize) * 100;
-    // readline.clearLine();
-    readline.clearLine(process.stdout, 0)
-    readline.cursorTo(process.stdout, 0, null)
-    process.stdout.write(`${Math.round(progress)}% complete`);
-   },
-  }, function (err, file) {
-   if (err) {
-    // Handle error
-    console.log(`ERROR WITH ${filename}`);
-    console.error(err);
-    reject(err)
-   } else {
-    console.log('File saved: ', filename);
-    resolve(file)
-   }
-  })
- })
-}
-
-
-
-// onUploadProgress: evt => {
-//   const progress = (evt.bytesRead / fileSize) * 100;
-//   readline.clearLine();
-//   readline.cursorTo(0);
-//   process.stdout.write(`${Math.round(progress)}% complete`);
-// },
-
 module.exports = {
  startProcess,
 };
+
+
+
+
+
+
+
+
+
+
+
