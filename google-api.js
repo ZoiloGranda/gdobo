9b3ec11e0be9856ddd31773fa4081806b1ddd9d5
Zoilo Granda <zgranda@gmail.com>
2020-03-13 15:58:57 -0400

    Fixed token.json not writing data

--- google-api.js
+++ google-api.js
@@ -75,8 +75,8 @@ function authorize(credentials, callback) {
     if (err) return console.error('Error retrieving access token', err);
     oAuth2Client.setCredentials(token);
     // Store the token to disk for later program executions
-    fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-     if (err) return console.error(err);
+    fs.writeFileSync(TOKEN_PATH, JSON.stringify(token), (err) => {
+     if (err) return console.error('errorcito',err);
      console.log('Token stored to', TOKEN_PATH);
     });
     callback(oAuth2Client);
