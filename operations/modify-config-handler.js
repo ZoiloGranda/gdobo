530d773294055fb495165fcbc9013a4ab8e80632
Zoilo Granda <zgranda@gmail.com>
2021-01-30 11:54:00 -0400

    fix indent removed console.log

--- operations/modify-config-handler.js
+++ operations/modify-config-handler.js
@@ -17,16 +17,15 @@ const removeFolder = require('./modify-config-ops/remove-folder')
 
 
 module.exports = async function modifyConfigHandler({ auth }) {
-  const operation = await askConfigOperation()
-  switch (operation) {
-    case 'ADD':
-      await addFolder({auth})
-      break;
-    case 'REMOVE':
-      await removeFolder({auth})
-      break;
-    default:
-      break;
-  }
-  console.log(operation)
+ const operation = await askConfigOperation()
+ switch (operation) {
+  case 'ADD':
+   await addFolder({ auth })
+   break;
+  case 'REMOVE':
+   await removeFolder({ auth })
+   break;
+  default:
+   break;
  }
+}
